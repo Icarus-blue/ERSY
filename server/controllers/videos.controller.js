9c6c439c2bc282664c18bd4b535d61dc61800640
henritresor <shimwamanahenritresor@gmail.com>
2024-03-02 19:56:57 +0200

    finished query operation

--- server/controllers/videos.controller.js
+++ server/controllers/videos.controller.js
@@ -2,11 +2,16 @@ import expressAsyncHandler from "express-async-handler";
 import client from "../utils/client.js";
 
 export const getMusicVideos = expressAsyncHandler(async (req, res, next) => {
-    const { page, pageSize } = req.query
+    const { page, pageSize, query } = req.query
     const videos = await client.videos.findMany({
         take: parseInt(pageSize),
         skip: (page - 1) * pageSize,
-        distinct: ['title']
+        distinct: ['title'],
+        where: {
+            title: {
+                contains: query
+            }
+        }
     });
 
     res.status(200).json({
