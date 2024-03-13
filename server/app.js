ae74171a4e2824ace772609826ca3f84f9fde3c5
henritresor <shimwamanahenritresor@gmail.com>
2024-03-13 07:46:57 +0200

    added cors support

--- server/app.js
+++ server/app.js
@@ -27,7 +27,12 @@ const server = http.createServer(app)
 const port = process.env.PORT || 8000
 
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: '*',
+    credentials: false
+  }
+))
 app.use(logger('dev'));
 app.use(express.json({
   limit: '35mb'
